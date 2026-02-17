702b979f221772023d6f04a369fe8587fc575abb
louloutte1302 <lou.pepin1@gmail.com>
2026-02-17 17:58:42 +0100

    v2.Voice caracteristic.rms

--- src/embed_catalog.py
+++ src/embed_catalog.py
@@ -18,3 +18,37 @@ def load_audio(audio_path, target_sr):
 
     return y, sr
 
+
+def calculation_pitch(y, sr):
+    """
+    Calcule le pitch (F0) avec librosa.pyin.
+    Renvoie un tableau f0 (avec des NaN quand il n'y a pas de voix détectée).
+    """
+    f0,_,_ = librosa.pyin(y, fmin=fmin, fmax=fmax)
+
+    # Suppr NaN
+    f0_clean = []
+    for value in f0:
+        if not np.isnan(value):
+            f0_clean.append(value)
+
+    if len(f0_clean) == 0:
+        return None  # pas de voix
+    
+    pitch_mean = sum(f0_clean) / len(f0_clean)
+
+    return pitch_mean 
+
+
+
+
+
+def compute_rms_mean(y):
+    """
+    Calcule l'énergie RMS moyenne (intensité moyenne).
+    """
+    rms_matrix = librosa.feature.rms(y=y)
+    rms_mean = float(np.mean(rms_matrix)) #prsq rms renvoie un rms pour chaque frame découpé donc on prend une seule valeur moy 
+    return rms_mean
+
+
